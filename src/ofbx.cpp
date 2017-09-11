21a041ade64812cf5d047863027c15a720b65eff
Martin Wickham <spexguy070@gmail.com>
2017-09-11 16:57:09 -0500

    Fix inverted post rotation

--- src/ofbx.cpp
+++ src/ofbx.cpp
@@ -2609,7 +2609,7 @@ Matrix Object::evalLocal(const Vec3& translation, const Vec3& rotation) const
 
 	Matrix r = getRotationMatrix(rotation, rotation_order);
 	Matrix r_pre = getRotationMatrix(getPreRotation(), RotationOrder::EULER_XYZ);
-	Matrix r_post_inv = getRotationMatrix(getPostRotation(), RotationOrder::EULER_XYZ);
+	Matrix r_post_inv = getRotationMatrix(-getPostRotation(), RotationOrder::EULER_ZYX);
 
 	Matrix r_off = makeIdentity();
 	setTranslation(getRotationOffset(), &r_off);
